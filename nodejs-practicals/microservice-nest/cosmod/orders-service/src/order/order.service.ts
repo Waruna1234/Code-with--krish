51ef1b09616e49ee230d29d68d8b3c8d5591aca7
waruna <warunaselaka1994@gmail.com>
2025-03-03 03:36:18 +0530

    Order Validate

--- nodejs-practicals/microservice-nest/cosmod/orders-service/src/order/order.service.ts
+++ nodejs-practicals/microservice-nest/cosmod/orders-service/src/order/order.service.ts
@@ -5,6 +5,9 @@ import { Repository } from 'typeorm';
 import { OrderItem } from './entity/order-item.entity';
 import { createOrderDto } from './dto/create-order.dto';
 import {OrderStatus,updateOrderStatusDto} from './dto/update-order.dto';
+import { CustomerDetails } from './entity/custormer.entity';
+import { Product } from './entity/product.entity';
+import { error } from 'console';
 
 @Injectable()
 export class OrderService {
@@ -13,11 +16,40 @@ export class OrderService {
          private orderRepository: Repository<Order>,
         @InjectRepository(OrderItem) 
         private ordetItemRepository: Repository<OrderItem>,
+        @InjectRepository(CustomerDetails)
+        private readonly customerRepository:Repository<CustomerDetails>,
+        @InjectRepository(Product)
+        private readonly productRepository:Repository<Product>,
+
 ){}
 
+   
 
 async create(createOrderDto:createOrderDto): Promise<Order | null>{
+    
+
+
+
     const{customerId, items} = createOrderDto;
+
+    const itemIds = createOrderDto.items.map(item =>  ({
+        productId: item.productId,
+        quantity: item.quantity
+      }));
+
+   
+      
+    const existsCus = this.findCutomer(customerId);
+    if(!existsCus){
+        throw new NotFoundException(`Customer not exist ${existsCus} not found`);
+        
+    }
+    const qtyExcist = this.findItems(items);
+    if(!qtyExcist){
+        throw new NotFoundException(`does not have enough stock.`);
+    }
+
+
 
     const order=this.orderRepository.create({
         customerId,
@@ -71,6 +103,33 @@ async updateOrderStatusDto(id:number,updateStatus:updateOrderStatusDto){
     return await this.orderRepository.save(order);
 
 }
+
+
+findCutomer(id){
+    
+    const existCustomer = this.customerRepository.findOne({
+        where: { id }
+      });
+    
+return existCustomer;
+
+}
+findItems(itemIds){
+
+    for(const item of itemIds){
+        const itest=this.productRepository.findOne({
+            where: { id: item.productId }
+          });        
+          if (item.quantity < itest.quantity){
+            return false;
+            break;
+          }        
+    }
+    return true;
+}
+
+
+
 
 }
 
